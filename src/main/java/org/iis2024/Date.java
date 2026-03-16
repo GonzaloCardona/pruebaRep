4d21cd6a35830f9d19af62a4de3b2b5a74d080ae
davidalonso-coder <david.alonso@uma.es>
2026-03-16 10:27:54 +0100

    Variable añadida

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,6 +19,8 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
+  private final int hola;
+  
 
   /**
    * Retrieves the day component of this date.
